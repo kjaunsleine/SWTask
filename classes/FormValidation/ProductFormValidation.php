9d6b4b16d9b480d2c56e9ec83fc61d3003815739
Krista Jaunsleine <krista.jaunsleine@inbox.lv>
2022-05-02 22:59:48 +0300

    Removed hardcoded properties Added autoload file Added use GetProducts class Removed RULE_INT validation rule for 'size' attribute Added RULE_SKU_MATCH validation rule for 'sku' attribute Added RULE_SKU_MATCH which checks if entered sku is already in database Added 'if property exists' check Removed isset($value) check Added methods that creates GetProducts object

--- classes/FormValidation/ProductFormValidation.php
+++ classes/FormValidation/ProductFormValidation.php
@@ -1,45 +1,12 @@
 <?php
 namespace SWTask\FormValidation;
 
+require '../vendor/autoload.php';
+
+use SWTask\GetProducts\GetProducts;
+
 class ProductFormValidation extends FormValidation
 {
-    /**
-     * @var string
-     */
-    protected $sku;
-    /**
-     * @var string
-     */
-    protected $name;
-    /**
-     * @var float
-     */
-    protected $price;
-    /**
-     * @var string
-     */
-    protected $productType;
-    /**
-     * @var int
-     */
-    protected $size;
-    /**
-     * @var float
-     */
-    protected $weight;
-    /**
-     * @var float
-     */
-    protected $width;
-    /**
-     * @var float
-     */
-    protected $length;
-    /**
-     * @var float
-     */
-    protected $height;
-
     /**
      * Get input value and set properties to 
      * corresponding attribute
@@ -49,10 +16,12 @@ class ProductFormValidation extends FormValidation
      * @return mixed
      */
     public function getValue($attribute)
-    {
-        return $this->{$attribute};
+    {   
+        if (isset($this->{$attribute})) {
+            return $this->{$attribute};
+        }
     }
-
+        
     /**
      * Establishes validation rules for attributes
      * 
@@ -61,11 +30,11 @@ class ProductFormValidation extends FormValidation
     public function rules()
     {
         return [
-            'sku' => [self::RULE_REQUIRED, self::RULE_SKU, [self::RULE_CHARS_MAX, 'max' => 100]],
+            'sku' => [self::RULE_REQUIRED, self::RULE_SKU, [self::RULE_CHARS_MAX, 'max' => 100], self::RULE_SKU_MATCH],
             'name' => [self::RULE_REQUIRED, [self::RULE_CHARS_MAX, 'max' => 100]],
             'price' => [self::RULE_REQUIRED, self::RULE_NUMBER, [self::RULE_MIN, 'min' => 0], [self::RULE_MAX, 'max' => 1000000]],
             'productType' => [self::RULE_REQUIRED],
-            'size' => [self::RULE_REQUIRED, self::RULE_INT, [self::RULE_MIN, 'min' => 0], [self::RULE_MAX, 'max' => 1000000]],
+            'size' => [self::RULE_REQUIRED, self::RULE_NUMBER, [self::RULE_MIN, 'min' => 0], [self::RULE_MAX, 'max' => 1000000]],
             'weight' => [self::RULE_REQUIRED, self::RULE_NUMBER, [self::RULE_MIN, 'min' => 0], [self::RULE_MAX, 'max' => 1000000]],
             'width' => [self::RULE_REQUIRED, self::RULE_NUMBER, [self::RULE_MIN, 'min' => 0], [self::RULE_MAX, 'max' => 1000000]],
             'length' => [self::RULE_REQUIRED, self::RULE_NUMBER, [self::RULE_MIN, 'min' => 0], [self::RULE_MAX, 'max' => 1000000]],
@@ -77,6 +46,7 @@ class ProductFormValidation extends FormValidation
      * Validates inputs
      * 
      * Iterates through validation rules array as attribute -> rules pairs.
+     * Checks if property of corresponding attribute exists, if true - gets value from property.
      * Then iterates through rules array. Sets $ruleName as rule if it is a string.
      * If it is not a string, sets $ruleName as the first item of an array (the second item is additional parameter).
      * Then checks the rules with corresponding condition (if value is defined, checks with regex etc.)
@@ -88,46 +58,54 @@ class ProductFormValidation extends FormValidation
     public function validate()
     {
         foreach ($this->rules() as $attribute => $rules) {
-            $value = $this->{$attribute};
+            if (property_exists($this, $attribute)) {
+                $value = $this->{$attribute};
+            }
             foreach ($rules as $rule) {
                 $ruleName = $rule;
                 if (!is_string($ruleName)) {
                     $ruleName = $rule[0];
                 }
-
-                if (isset($value)) {
-                    if ($ruleName === self::RULE_REQUIRED && !$value) {
-                        $this->addError($attribute, self::RULE_REQUIRED);
-                    }
-    
-                    if ($ruleName === self::RULE_SKU && !preg_match("/^([0-9a-zA-Z])+$/", $value)) {
-                        $this->addError($attribute, self::RULE_SKU);
-                    }
-    
-                    if ($ruleName === self::RULE_CHARS_MAX && strlen($value) > $rule['max']) {
-                        $this->addError($attribute, self::RULE_CHARS_MAX, $rule);
-                    }
-    
-                    if ($ruleName === self::RULE_NUMBER && !preg_match("/^(\d+|\d+\.\d{1,2})$/", $value)) {
-                        $this->addError($attribute, self::RULE_NUMBER);
-                    }
-    
-                    if ($ruleName === self::RULE_INT && !filter_var($value, FILTER_VALIDATE_INT)) {
-                        $this->addError($attribute, self::RULE_INT);
-                    }
-    
-                    if ($ruleName === self::RULE_MIN && $value < $rule['min']) {
-                        $this->addError($attribute, self::RULE_MIN, $rule);
-                    }
-    
-                    if ($ruleName === self::RULE_MAX && $value > $rule['max']) {
-                        $this->addError($attribute, self::RULE_MAX, $rule);
-                    }
-                }
                 
-            }
+                if ($ruleName === self::RULE_REQUIRED && !$value) {
+                    $this->addError($attribute, self::RULE_REQUIRED);
+                }
 
+                if ($ruleName === self::RULE_SKU && !preg_match("/^([0-9a-zA-Z])+$/", $value)) {
+                    $this->addError($attribute, self::RULE_SKU);
+                }
+
+                if ($ruleName === self::RULE_SKU_MATCH && $this->getSavedProducts()->checkSku($value)) {
+                    $this->addError($attribute, self::RULE_SKU_MATCH);
+                }
+
+                if ($ruleName === self::RULE_CHARS_MAX && strlen($value) > $rule['max']) {
+                    $this->addError($attribute, self::RULE_CHARS_MAX, $rule);
+                }
+
+                if ($ruleName === self::RULE_NUMBER && !preg_match("/^(\d+|\d+\.\d{1,2})$/", $value)) {
+                    $this->addError($attribute, self::RULE_NUMBER);
+                }
+
+                if ($ruleName === self::RULE_MIN && $value < $rule['min']) {
+                    $this->addError($attribute, self::RULE_MIN, $rule);
+                }
+
+                if ($ruleName === self::RULE_MAX && $value > $rule['max']) {
+                    $this->addError($attribute, self::RULE_MAX, $rule);
+                }                
+            }
         }
         return empty($this->errors);
+    }
+
+    /**
+     * Gets products savedy database
+     * 
+     * @return object
+     */
+    public function getSavedProducts()
+    {
+        return new GetProducts;
     }
 }
