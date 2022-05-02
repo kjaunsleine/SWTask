f159e0cc4d60ddf4a688664f44ca54d70f4e8527
Krista Jaunsleine <krista.jaunsleine@inbox.lv>
2022-05-02 23:17:12 +0300

    Moved dropdown change from app.js Imported Fields class Created option change using Fields object properties Removed 'size' attribute validation rule Created method that gets first error string from errors array Added ajax submit method which gets errors array from data or redirects

--- js/form.js
+++ js/form.js
@@ -1,12 +1,34 @@
-/* Form validation with jQuery validation */
+import Fields from '../js/fields.js';
 
 $(function(){
 
     'use strict';
 
+    const productFields = new Fields();
+
+    /* Change form depending on type */
+    const $productFields = $('#productFields');
+    $('#productType').change(function() {
+        $('#productType option').each(function() {
+            let typeOption = $(this).attr('value');
+            if (!typeOption) {
+                return;
+            } else {
+                $productFields.html('');
+            }
+        });
+        let typeSelected = $('#productType option:selected').attr('value');
+        if (!typeSelected) {
+        return;
+        } else {
+            $productFields.html(productFields[typeSelected + 'Fields']);
+        }
+    }).change();
+
     const $productForm = $('#product_form');
 
-    /* Validation */
+
+    /* Form validation with jQuery validation */
 
     // validate SKU against regex
     $.validator.addMethod('skuRegex', function(value, element){
@@ -30,21 +52,41 @@ $(function(){
         sku: {required: true, maxlength: 100, skuRegex: true },
         name: {required: true, maxlength: 100},
         productType: 'required',
-        size: {required: true, digits: true, min: 0, max: 1000000},
     },
     messages: {
         productType: {required: 'Product type must be chosen'}
     }
     });
 
-    /* Submit form */
+    /* Submit form with AJAX*/
 
-    $('#save-btn').click(function() {
-        $productForm.validate({
-            submitHandler: function(form) {
-                $(form).submit();
-            }
-        });
-    }); 
+    const getFirstError = (attribute, errors) => {
+        let error = errors[attribute][0] ? errors[attribute][0] : '';
+        let attributeIdError = $(`#${attribute}-error`);
+        $(attributeIdError).show().html(error);
+    }
 
+    $productForm.submit(function(event){
+        event.preventDefault();
+        if($productForm.valid()){
+            const dataString = $(this).serialize();
+            console.log(dataString);
+            $.ajax({
+                type: 'POST',
+                url: 'includes/addProduct.inc.php',
+                data: dataString,
+                dataType: 'json',
+                success: function(data){
+                    let errors = data['errors'];
+                    if (errors !== 0) {
+                        for (let attribute in errors) {
+                            getFirstError(attribute, errors);
+                        }
+                    } else {
+                        window.location.href = 'index.php';
+                    }
+                }
+            });
+        }
+    });
 });
