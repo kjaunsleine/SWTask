b9769ad1a30bfce18d0aad402e7cc5aae7c766c6
Krista Jaunsleine <krista.jaunsleine@inbox.lv>
2022-05-03 10:05:03 +0300

    Added extra spacing

--- js/form.js
+++ js/form.js
@@ -32,12 +32,12 @@ $(function(){
 
     // validate SKU against regex
     $.validator.addMethod('skuRegex', function(value, element){
-    return this.optional(element) || /^([0-9a-zA-Z])+$/.test(value);
+        return this.optional(element) || /^([0-9a-zA-Z])+$/.test(value);
     }, 'SKU can contain only letters or numbers');
 
     // validate numerical attributes against regex
     $.validator.addMethod('productAttributeRegex', function(value, element){
-    return this.optional(element) || /^(\d+|\d+\.\d{1,2})$/.test(value);
+        return this.optional(element) || /^(\d+|\d+\.\d{1,2})$/.test(value);
     }, 'Decimal number in format X.XX with max two digits after decimal point');
 
     $.validator.addClassRules('formAttributes', {
@@ -48,23 +48,25 @@ $(function(){
     });
 
     $productForm.validate({
-    rules: {
-        sku: {required: true, maxlength: 100, skuRegex: true },
-        name: {required: true, maxlength: 100},
-        productType: 'required',
-    },
-    messages: {
-        productType: {required: 'Product type must be chosen'}
-    }
+        rules: {
+            sku: {required: true, maxlength: 100, skuRegex: true },
+            name: {required: true, maxlength: 100},
+            productType: 'required',
+        },
+        messages: {
+            productType: {required: 'Product type must be chosen'}
+        }
     });
 
-    /* Submit form with AJAX*/
+    /* Get first error from errors array */
 
     const getFirstError = (attribute, errors) => {
         let error = errors[attribute][0] ? errors[attribute][0] : '';
         let attributeIdError = $(`#${attribute}-error`);
         $(attributeIdError).show().html(error);
     }
+
+    /* Submit form with AJAX*/
 
     $productForm.submit(function(event){
         event.preventDefault();
