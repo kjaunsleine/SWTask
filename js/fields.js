384ec61999d73ca62edb0083d82de83ea5c89a01
Krista Jaunsleine <krista.jaunsleine@inbox.lv>
2022-05-03 16:31:34 +0300

    Moved product description before the input fields

--- js/fields.js
+++ js/fields.js
@@ -7,10 +7,10 @@ export default class Fields {
         let html = '<div class="container"><fieldset id="Book" class="row row-cols-12 row-cols-md-6 flex-column flex-sm-row">'
         html += '<label class="col form-label" for="weight">Weight (KG)</label>';
         html += '<div class="col col-md-6 col-lg-4">';
+        html += '<label class="form-text">Please provide weight in KG</label>';
         html += '<input id="weight" class="form-control formValue formAttributes" type="number" step="any" name="weight">';
-        html += '<label id="weight-error" class="error" for="weight"></label>';
-        html += '<label class="form-text mb-3">Please provide weight in KG</label>';
-        html += '</div></fieldset></div>'
+        html += '<label id="weight-error" class="error" for="weight"></label></div>';
+        html += '</fieldset></div>'
         return html;
     }
 
@@ -18,15 +18,16 @@ export default class Fields {
         let html = '<div class="container"><fieldset id="DVD" class="row row-cols-12 row-cols-md-6 flex-column flex-sm-row">';
         html += '<label class="col-2 col-form-label" for="size">Size (MB)</label>';
         html += '<div class="col col-md-6 col-lg-4">';
+        html += '<label class="form-text mb-3">Please provide size in MB</label>';
         html += '<input id="size" class="form-control formValue formAttributes" type="number" step="any" name="size">';
         html += '<label id="size-error" class="error" for="size"></label>';
-        html += '<label class="form-text mb-3">Please provide size in MB</label>';
         html += '</div></fieldset></div>';
         return html;
     }
 
     createFurnitureFields() {
         let html = '<div class="container"><fieldset id="Furniture" class="row">';
+        html += '<label class="form-text mb-3">Please provide dimensions in CM and in HxWxL format</label>';
         html += '<div class="row row-cols-md-6 flex-column flex-sm-row">';
         html += '<label class="col col-form-label" for="height">Height (CM)</label>'
         html += '<div class="col col-md-6 col-lg-4">';
@@ -44,7 +45,7 @@ export default class Fields {
         html += '<div class="col col-md-6 col-lg-4">';
         html += '<input id="length" class="form-control formAttributes" type="number" name="length" step="any" value="">';
         html += '<label id="length-error" class="error" for="length"></label></div></div>';
-        html += '<label class="form-text mb-3">Please provide dimensions in CM and in HxWxL format</label>';
+        
         html += '</div></fieldset></div>';
     return html;
     }
