460d1fd7a0d6c985f847f1c6d4f80d3a8fe81baa
Krista Jaunsleine <krista.jaunsleine@inbox.lv>
2022-05-29 21:40:27 +0300

    Added JS and CSS files

---
+++ public/js/form.js
@@ -0,0 +1,49 @@
+$(function(){
+
+    'use strict';
+
+    const url = 'http://localhost:8000/add/fields';
+
+    $('#productType').on('change', function(){
+        const dataString = $(this).serialize();
+        console.log(dataString);
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: dataString,
+            success: function(data){
+                    $('#productFields').html(data);
+            }
+        });
+    });
+
+    /* Form validation with jQuery validation */
+
+    // validate SKU against regex
+    $.validator.addMethod('skuRegex', function(value, element){
+        return this.optional(element) || /^([0-9a-zA-Z])+$/.test(value);
+    }, 'SKU can contain only letters or numbers');
+
+    // validate numerical attributes against regex
+    $.validator.addMethod('productAttributeRegex', function(value, element){
+        return this.optional(element) || /^(\d+|\d+\.\d{1,2})$/.test(value);
+    }, 'Decimal number in format X.XX with max two digits after decimal point');
+
+    $.validator.addClassRules('formAttributes', {
+        required: true,
+        min: 0,
+        max: 1000000,
+        productAttributeRegex: true
+    });
+
+    $('#product_form').validate({
+        rules: {
+            sku: {required: true, maxlength: 100, skuRegex: true },
+            name: {required: true, maxlength: 100},
+            productType: 'required',
+        },
+        messages: {
+            productType: {required: 'Product type must be chosen'}
+        }
+    });
+});
